72cab457426bb2eef2d2520ea4939d7adedb34a5
Kevin Otten <kevin.otten@ziggo.nl>
2018-09-11 16:53:20 +0200

    kleine fix voor het snappen van rotatie van camera naar 0

--- Jaar3Project1Devlab/Assets/Personal/Kevin/Scripts/Movement.cs
+++ Jaar3Project1Devlab/Assets/Personal/Kevin/Scripts/Movement.cs
@@ -7,10 +7,15 @@ public class Movement : MonoBehaviour {
 	public float moveSpeed;
 	public float rotSpeed;
 
-	private float yRotInput;
+	private float xRotInput;
 	public float clampValue;
 
 	public bool canMove;
+
+	void Start()
+	{
+		xRotInput = 20;
+	}
 
 	void FixedUpdate ()
 	{
@@ -32,13 +37,13 @@ public class Movement : MonoBehaviour {
 
 	public void SoldierRotation()
 	{
-		float xRotInput = Input.GetAxis("Mouse X") * Time.deltaTime * rotSpeed;
-		transform.Rotate(0, xRotInput, 0);
+		float yRotInput = Input.GetAxis("Mouse X") * Time.deltaTime * rotSpeed;
+		transform.Rotate(0, yRotInput, 0);
 
-		yRotInput -= Input.GetAxis("Mouse Y") * Time.deltaTime * rotSpeed;
-		print (yRotInput);
-		yRotInput = Mathf.Clamp(yRotInput, -clampValue, clampValue);
-		Camera.main.transform.localRotation = Quaternion.Euler(yRotInput, 0, 0);
+		xRotInput -= Input.GetAxis("Mouse Y") * Time.deltaTime * rotSpeed;
+		print (xRotInput);
+		xRotInput = Mathf.Clamp(xRotInput, -clampValue, clampValue);
+		Camera.main.transform.localRotation = Quaternion.Euler(xRotInput, 0, 0);
 	}
 
 }
