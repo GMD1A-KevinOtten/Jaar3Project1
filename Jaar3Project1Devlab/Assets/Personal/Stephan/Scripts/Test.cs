d0f064cd5c3b59fe80cf4dd26aa8ed4e729a2950
StephanHoldingDeltion <stephan@holdingsnel.nl>
2018-09-17 22:24:15 +0200

    Bullet casing particle added

---
+++ Jaar3Project1Devlab/Assets/Personal/Stephan/Scripts/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour {
+
+    public RaycastHit hit;
+    public Image image;
+    public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.DrawRay(transform.position, transform.forward * 20, Color.red);
+        Physics.Raycast(transform.position, transform.forward, out hit, 20);
+
+        image.transform.position = cam.WorldToScreenPoint(hit.point);
+	}
+}
