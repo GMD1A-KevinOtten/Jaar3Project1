932cfad41806e8ef18adb65a4a28708361d53ba1
Kevin Otten <kevin.otten@ziggo.nl>
2018-09-26 16:58:56 +0200

    snip prefab en wat in mijn scene]

--- Jaar3Project1Devlab/Assets/Personal/Kevin/Scripts/Sniper.cs
+++ Jaar3Project1Devlab/Assets/Personal/Kevin/Scripts/Sniper.cs
@@ -4,5 +4,44 @@ using UnityEngine;
 
 public class Sniper : Weapon {
 
-	
+    [Header("Sniper Specific")]
+
+    private float baseFovValue;
+    public float fovValue;
+
+    void Start() 
+    {
+        baseFovValue = Camera.main.GetComponent<Camera>().fieldOfView;
+    }
+
+	void Update() 
+    {
+        if(gameObject.transform.root.GetComponent<Soldier>() != null)
+        {
+            if(currentClip == 0)
+            {
+                StartCoroutine(waitTillTrigger());
+            }
+            if(gameObject.transform.root.GetComponent<Soldier>().isActive == true)
+            {
+                if(Input.GetButtonDown("Fire2"))
+                {
+                    ActivateScope();
+                }
+            }
+        }
+    }
+
+    public IEnumerator waitTillTrigger()
+    {
+        yield return new WaitForSeconds(3);
+        TeamManager.instance.NextTeam();
+    }
+
+    public void ActivateScope()
+    {
+        Camera.main.GetComponent<Camera>().fieldOfView = fovValue; 
+        //Scope UI element
+        //Play sound
+    }
 }
