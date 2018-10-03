7f9564fdb22c69e70a9128bbd13ee35314eb0c95
gmd1apeterbos <97040824@st.deltion.nl>
2018-10-03 11:48:10 +0200

    Tank with projectile dysfunction

---
+++ Jaar3Project1Devlab/Assets/Personal/Peter/Scripts/Tank.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : InteractableObject {
+    public float sphereRadius = 5;
+    public Transform soldierInsidePos;
+
+    public Camera barrelCam;
+    public GameObject turret;
+    public GameObject barrel;
+    public float barrelRotationSpeed;
+
+    public float screenBoundary;
+    private Vector3 soldierOutsidePos;
+    private Soldier currentSoldier;
+    private bool soldierInside;
+
+    [Header("Clamp properties")]
+    public bool clamp;
+    public float clampX;
+    public float clampY;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Interact();
+	}
+
+    private void FixedUpdate()
+    {
+        if (soldierInside)
+        {
+            Movement();
+        }
+    }
+
+    void Movement()
+    {
+        Vector2 mousePos = Input.mousePosition;
+
+        if(mousePos.x > Screen.width - screenBoundary)
+        {
+            //Right side
+            turret.transform.Rotate(transform.up * barrelRotationSpeed * Time.deltaTime);
+        }
+        if (mousePos.x < 0 + screenBoundary)
+        {
+            //Left side
+            turret.transform.Rotate(-transform.up * barrelRotationSpeed * Time.deltaTime);
+        }
+        if (mousePos.y > Screen.height - screenBoundary)
+        {
+            //Top side
+            barrel.transform.Rotate(transform.up * barrelRotationSpeed * Time.deltaTime);
+        }
+        if (mousePos.y < 0 + screenBoundary)
+        {
+            //Bottom side
+            barrel.transform.Rotate(-transform.up * barrelRotationSpeed * Time.deltaTime);
+        }
+
+    }
+
+    public override void Interact()
+    {
+        if (soldierNearby())
+        {
+            if (Input.GetKeyDown("e"))
+            {
+                soldierInsidePos.position = currentSoldier.gameObject.transform.position;
+                currentSoldier.gameObject.transform.position = soldierInsidePos.position;
+                currentSoldier.isActive = false;
+                currentSoldier.soldierMovement.canMove = false;
+
+                Camera.main.enabled = false;
+                barrelCam.enabled = true;
+                soldierInside = true;
+
+                
+            }
+        }
+    }
+
+    public bool soldierNearby()
+    {
+        Collider[] cols = Physics.OverlapSphere(transform.position, sphereRadius);
+        foreach (Collider c in cols)
+        {
+            if (c.GetComponentInParent<Soldier>())
+            {
+                if (c.GetComponentInParent<Soldier>().isActive)
+                {
+                    currentSoldier = c.GetComponentInParent<Soldier>();
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
