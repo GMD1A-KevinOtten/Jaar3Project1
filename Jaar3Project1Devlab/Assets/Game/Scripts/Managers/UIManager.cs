726b6531ae86df1283c487f5927013fd419a9d47
StephanHoldingDeltion <stephan@holdingsnel.nl>
2018-10-04 12:27:21 +0200

    UI manager start

--- Jaar3Project1Devlab/Assets/Game/Scripts/Managers/UIManager.cs
+++ Jaar3Project1Devlab/Assets/Game/Scripts/Managers/UIManager.cs
@@ -8,6 +8,13 @@ public class UIManager : MonoBehaviour {
     public static UIManager instance;
     public Image crosshairImage;
     public GameObject sniperScope;
+    public UI_SoldierStatus[] ui_soldierStatuses;
+
+    [Header("Prefabs")]
+    public GameObject soldierStatusPrefab;
+
+    [Header("Parents")]
+    public RectTransform soldierStatusParent;
 
     public bool showCroshair = true;
 
@@ -21,6 +28,32 @@ public class UIManager : MonoBehaviour {
         else
         {
             Destroy(gameObject);
+        }
+    }
+
+    public UI_SoldierStatus[] InstantiateSoldierStatus(List<Team> teams)
+    {
+        List<UI_SoldierStatus> toReturn = new List<UI_SoldierStatus>();
+
+        foreach(Team t in teams)
+        {
+            for (int i = 0; i < t.allSoldiers.Count; i++)
+            {
+                GameObject newObject = Instantiate(soldierStatusPrefab, soldierStatusParent.transform.position, soldierStatusPrefab.transform.rotation);
+                newObject.transform.SetParent(soldierStatusParent, false);
+
+                toReturn.Add(newObject.GetComponent<UI_SoldierStatus>());
+            }
+        }
+
+        return toReturn.ToArray();
+    }
+
+    public void UpdateSoldierStatuses()
+    {
+        foreach(UI_SoldierStatus status in ui_soldierStatuses)
+        {
+            status.UpdateStatus();
         }
     }
 
---
+++ Jaar3Project1Devlab/Assets/Game/Scripts/UI/UI_SoldierStatus.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_SoldierStatus : MonoBehaviour {
+
+    private Soldier soldier;
+
+    public Image healthBar;
+    public Image deathMarker;
+    public bool alive;
+
+    private void Awake()
+    {
+        soldier.transform.root.GetComponent<Soldier>();
+        UpdateStatus();
+    }
+
+    public void UpdateStatus()
+    {
+        if (soldier.health <= 0 || soldier.isDead)
+        {
+            deathMarker.gameObject.SetActive(true);
+            alive = false;
+        }
+
+        float percent = soldier.health / 100;
+        healthBar.fillAmount = percent;
+    }
+}
