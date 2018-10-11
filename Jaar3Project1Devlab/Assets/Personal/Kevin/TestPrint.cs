e2a0e1681602a3527893d1682c7daa66f42fbe17
Kevin Otten <kevin.otten@ziggo.nl>
2018-10-11 11:48:15 +0200

    ragdoll progress

---
+++ Jaar3Project1Devlab/Assets/Personal/Kevin/TestPrint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPrint : MonoBehaviour {
+
+	private void OnCollisionEnter(Collision other) 
+	{
+		if(other.transform.GetComponent<Bullet>())
+		{
+			print("Wowzers");
+		}
+	}
+}
