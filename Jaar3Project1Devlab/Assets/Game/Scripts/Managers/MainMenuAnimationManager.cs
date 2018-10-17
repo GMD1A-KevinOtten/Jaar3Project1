140acc0b605ea4e8da14acfc7e0a05f45a0b5c0a
gmd1apeterbos <97040824@st.deltion.nl>
2018-10-17 13:30:58 +0200

    Main menu

---
+++ Jaar3Project1Devlab/Assets/Game/Scripts/Managers/MainMenuAnimationManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuAnimationManager : MonoBehaviour {
+
+    public Animator mainCamAnim;
+    public Animator tankAnim;
+
+    public GameObject optionsCanvas;
+    private bool quitting;
+    private void Awake()
+    {
+        mainCamAnim = Camera.main.GetComponent<Animator>();
+        if(tankAnim == null)
+        {
+            Debug.LogError("Please assign tankAnim in the inspector");
+        }
+    }
+
+    public void OptionsToggle()
+    {
+        if (!mainCamAnim.GetBool("MoveToOptions"))
+        {
+            mainCamAnim.SetBool("MoveToOptions", true);
+        }
+        else if (mainCamAnim.GetBool("MoveToOptions"))
+        {
+            mainCamAnim.SetBool("MoveToOptions", false);
+        }
+    }
+
+    public void QuitGameCamAnimation()
+    {
+        if (!quitting)
+        {
+            quitting = true;
+            optionsCanvas.SetActive(false);
+            mainCamAnim.SetBool("QuitGame", true);
+            StartCoroutine(QuitGameTankAnimation());
+        }
+      
+    }
+    public IEnumerator QuitGameTankAnimation()
+    {
+        yield return new WaitForSeconds(1);
+        tankAnim.SetBool("QuitGame", true);
+        StartCoroutine(QuitGame());
+    }
+
+    public IEnumerator QuitGame()
+    {
+        yield return new WaitForSeconds(1.3F);
+#if UNITY_EDITOR
+
+        UnityEditor.EditorApplication.isPlaying = false;
+
+#endif
+        Application.Quit();
+
+    }
+}
---
+++ Jaar3Project1Devlab/Assets/Game/Scripts/Managers/SettingsManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using TMPro;
+
+public class SettingsManager : MonoBehaviour {
+
+    public AudioMixer mainMixer;
+
+    public TMP_Dropdown resolutionDropdown;
+    public TMP_Dropdown qualityDropdown;
+    private List<Resolution> resolutions = new List<Resolution>();
+
+    private void Start()
+    {
+        FindResolutions();
+        FindQuality();
+
+    }
+
+    public void SetVolume(float volume)
+    {
+        mainMixer.SetFloat("volume", volume);
+    }
+
+    public void ToggleFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        Debug.Log(Screen.fullScreen);
+    }
+
+    public void ChangeResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void ChangeQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    void FindResolutions()
+    {
+        foreach (Resolution r in Screen.resolutions)
+        {
+            resolutions.Add(r);
+        }
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        foreach (Resolution res in resolutions)
+        {
+            string s = res.width + " x " + res.height;
+            options.Add(s);
+
+            if (res.width == Screen.currentResolution.width && res.height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = resolutions.IndexOf(res);
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    void FindQuality()
+    {
+        qualityDropdown.value = QualitySettings.GetQualityLevel();
+        qualityDropdown.RefreshShownValue();
+    }
+}
