8c79928e20fa16e760dae7d357888df83151dff9
StephanHoldingDeltion <stephan@holdingsnel.nl>
2018-11-07 13:27:28 +0100

    Minefield added

---
+++ Jaar3Project1Devlab/Assets/Game/Scripts/CorrectPositionOnTerrain.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorrectPositionOnTerrain : MonoBehaviour {
+
+    private RaycastHit hit;
+
+    public void Awake()
+    {
+        if(Physics.Raycast(transform.position + new Vector3(0, 5, 0), -Vector3.up, out hit, 10))
+        {
+            if (hit.transform.GetComponent<Terrain>() != null)
+            {
+                print(transform.gameObject.name + ": " + hit.point);
+                transform.position = hit.point;
+                transform.rotation = Quaternion.LookRotation(transform.forward, hit.normal);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        Debug.DrawRay(transform.position + new Vector3(0, 5, 0), -Vector3.up * 10, Color.black);
+    }
+
+}
---
+++ Jaar3Project1Devlab/Assets/Game/Scripts/LightFlicker.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+
+    public Vector2 intensityMin_Max;
+    public Vector2 flickerDurationMin_Max;
+
+    private float intensity;
+    private float duration;
+    private float timer;
+    private bool ready;
+    private Light lightSource;
+
+    private void Awake()
+    {
+        lightSource = GetComponent<Light>();
+        ready = true;
+    }
+
+    private void Update()
+    {
+        if (ready)
+        {
+            intensity = Random.Range(intensityMin_Max.x, intensityMin_Max.y);
+            duration = Random.Range(flickerDurationMin_Max.x, flickerDurationMin_Max.y);
+
+            if (intensity > lightSource.intensity)
+                StartCoroutine(FlickerUp());
+            else
+                StartCoroutine(FlickerDown());
+        }
+    }
+
+    public IEnumerator FlickerUp()
+    {
+        ready = false;
+
+        while (lightSource.intensity < intensity)
+        {
+            lightSource.intensity += Time.deltaTime * duration;
+
+            yield return null;
+        }
+
+         ready = true;
+    }
+    public IEnumerator FlickerDown()
+    {
+        ready = false;
+
+        while (lightSource.intensity > intensity)
+        {
+            lightSource.intensity -= Time.deltaTime * duration;
+
+            yield return null;
+        }
+
+          ready = true;
+    }
+
+
+}
